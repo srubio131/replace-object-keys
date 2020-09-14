8249362d9e36d91429a3eaaed61a26a4166a8ce2
Sergio Rubio <sergio.rubio@waavi.com>
2020-09-14 17:16:42 +0200

    Refactor normalizeObject function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const snakeToCamelCase = (s) => s.replace(/(\_\w)/g, (m) => m[1].toUpperCase())
 
 // Change object keys format for camel case from snake case
-const normalizeObject = (obj) => {
+export const normalizeObject = (obj) => {
   const keys = Object.keys(obj)
   return keys.reduce((acc, key) => {
     const value = obj[key]
-    if(Object.prototype.toString.call(value) !== '[object Object]') {
-      return { ...acc, [snakeToCamelCase(key)]: value }
-    }
-    return { ...acc, [snakeToCamelCase(key)]: normalizeObject(value) }
+    const isObject = Object.prototype.toString.call(value) === '[object Object]'
+    return { ...acc, [snakeToCamelCase(key)]: (!isObject) ? value : normalizeObject(value) }
   }, {})
 }
 
